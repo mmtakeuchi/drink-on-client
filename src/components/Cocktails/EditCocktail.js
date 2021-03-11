255b8a5490185df138d50410c93b7abe84c46864
mmtakeuchi <mikemtaki@gmail.com>
2021-03-11 12:35:21 -0700

    added error handling to edit cocktial form

--- src/components/Cocktails/EditCocktail.js
+++ src/components/Cocktails/EditCocktail.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
 import { Redirect, useHistory, withRouter } from "react-router-dom";
+import { CloudinaryContext } from "cloudinary-react";
 import Grid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/core/styles";
 import FormControl from "@material-ui/core/FormControl";
@@ -36,49 +37,112 @@ const EditCocktail = (props) => {
   );
 
   const correctUser = props.user.current.id === drink.user_id;
-  console.log(props);
 
   const [values, setValues] = useState({
     id: `${drink.id}`,
     name: `${drink.name}`,
     image: `${drink.image}`,
     ingredients: `${drink.ingredients}`,
+    nameErrors: "",
+    ingredientError: "",
   });
 
   const handleInputChange = (e) => {
     setValues({ ...values, [e.target.name]: e.target.value });
   };
 
+  const validateInputs = () => {
+    let isValid = true;
+    let name = values.name;
+    let ingredients = values.ingredients;
+    let nameError = "";
+    let ingredientsError = "";
+
+    if (!values.name) {
+      nameError = "Invalid name";
+    }
+
+    if (!values.ingredients) {
+      ingredientsError = "Ingredients can't be empty.";
+    }
+
+    if (nameError || ingredientsError) {
+      setValues({ name, ingredients, nameError, ingredientsError });
+      isValid = false;
+    }
+
+    return isValid;
+  };
+
+  const imageSubmit = () => {
+    var myUploadWidget;
+    myUploadWidget = window.cloudinary.openUploadWidget(
+      {
+        cloudName: "disiodpgq",
+        uploadPreset: "ij6ulbcy",
+      },
+      (error, result) => {
+        console.log(result);
+        if (result.info.secure_url) {
+          setValues({
+            image: result.info.secure_url,
+          });
+        }
+      }
+    );
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    props.updateCocktail(values, props.user.current.id);
-    history.push(`/cocktails/${values.id}`);
+    if (validateInputs()) {
+      props.updateCocktail(values, props.user.current.id);
+      history.push(`/cocktails/${values.id}`);
+    }
   };
 
   const renderUser = () => {
+    // console.log(props);
     return correctUser ? (
       <React.Fragment>
         <div className={classes.root}>
           <h1>Update Cocktail</h1>
-          <form className={classes.form} noValidate autoComplete="off">
+          <CloudinaryContext cloudName="disiodpgq">
+            <button
+              id="upload_widget"
+              onClick={imageSubmit}
+              className="cloudinary-button"
+            >
+              Upload files
+            </button>
+          </CloudinaryContext>
+          <form
+            className={classes.form}
+            noValidate
+            onSubmit={handleSubmit}
+            autoComplete="off"
+          >
             <Grid container spacing={2}>
               <Grid item xs={12}>
-                <FormControl className={classes.inputField} error>
+                <FormControl
+                  className={classes.inputField}
+                  error={values.nameError ? true : null}
+                >
                   <InputLabel htmlFor="name">Name</InputLabel>
                   <Input
+                    error={values.nameError ? true : null}
                     name="name"
                     aria-describedby="my-helper-text"
                     value={values.name}
                     onChange={handleInputChange}
                   />
                   <FormHelperText id="my-helper-text">
-                    We'll never share your email.
+                    {values.nameError}
                   </FormHelperText>
                 </FormControl>
               </Grid>
               <Grid item xs={12}>
                 <FormControl
-                  error
+                  error={values.ingredientsError ? true : null}
                   className={classes.inputField}
                   variant="outlined"
                 >
@@ -86,41 +150,29 @@ const EditCocktail = (props) => {
                   <OutlinedInput
                     multiline
                     rows={5}
+                    error={values.ingredientsError ? true : null}
                     label="Ingredients"
                     id="component-error"
                     name="ingredients"
+                    placeholder="Please seperate ingredients with a comma"
                     value={values.ingredients}
                     onChange={handleInputChange}
                     aria-describedby="component-error-text"
                   />
                   <FormHelperText id="component-error-text">
-                    Please seperate ingredients by a ","
+                    {values.ingredientsError
+                      ? values.ingredientsError
+                      : "Please seperate ingredients with a comma"}
                   </FormHelperText>
                 </FormControl>
               </Grid>
 
               <Grid item xs={12}>
-                <input
-                  accept="image/*"
-                  name="image"
-                  // className={classes.inputField}
-                  style={{ width: "35%" }}
-                  id="contained-button-file"
-                  multiple
-                  type="file"
-                />
-                <label htmlFor="contained-button-file">
-                  <Button variant="contained" color="primary" component="span">
-                    Upload
-                  </Button>
-                </label>
-              </Grid>
-              <Grid item xs={12}>
                 <Button
                   variant="outlined"
+                  type="submit"
                   color="primary"
-                  className={classes.button}
-                  onClick={handleSubmit}
+                  // className={classes.button}
                   startIcon={<AddCircleOutlineIcon />}
                 >
                   Update Cocktail
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,11 +3,9 @@ import "./Home.css";
 import { Link } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
-import Cocktails from "./Cocktails/Cocktails";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -51,8 +49,7 @@ const Home = (props) => {
           gutterBottom
         >
           Welcome to Drink On!
-          <br /> Jump right in and explore the drinks.
-          <br /> Feel free to share some of your own and comment on others!
+          <br /> Explore all our drinks, and share your own cocktails!
         </Typography>
         <Button variant="contained" color="primary">
           <Link
